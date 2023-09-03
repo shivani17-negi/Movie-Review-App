ffe5dbf7709bab79dc8f916f3dfc2152da12514d
shivani <shivaninegi076@gmail.com>
2023-09-03 11:56:53 +0530

    preventing duplicate user

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,9 +2,11 @@ const User = require("../models/user");
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body;
-
+  const oldUser = await User.findOne({email});
+  if(oldUser) return res.status(401).json({error:"this email is already in use!"});
+  
   const newUser = new User({ name, email, password });
   await newUser.save();
 
-  res.json({ user: newUser });
+  res.status(201).json({ user: newUser });
 };
